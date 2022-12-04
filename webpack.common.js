ae73f953eb2150120b05b6d9a5737c0877a20b70
shayshahal <shay840@gmail.com>
2022-12-04 15:42:44 +0200

    webpack development

---
+++ webpack.common.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,  
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "To-Do",
+    }),
+  ],
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
