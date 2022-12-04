ba92d0b99e3e9585f5e58233c0b64a03dcba3934
shayshahal <shay840@gmail.com>
2022-12-04 15:42:54 +0200

    webpack development

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import './styles/page.css'
 import loadMenu from '/src/scripts/menu.js';
 import loadMain from '/src/scripts/main.js';
 
+if (process.env.NODE_ENV !== 'production') {
+    console.log('Looks like we are in development mode!');
+  }
+
 loadMenu();
 loadMain();
--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -2,34 +2,44 @@ import '/src/styles/menu.css';
 
 export default () =>
 {
-    //--------------------------Create top menu --------------------------------------
+    //--------------------------Create top menu --------------------------------------//
     const topMenu = createElement('div', 'top-menu','');
     document.body.appendChild(topMenu);
+
     // Create Menu expending button
     const menuExpandButton = createElement('button', 'menu-expand-btn', '☰');
     topMenu.appendChild(menuExpandButton);
 
-
-    //--------------------------Create left menu --------------------------------------
+    //--------------------------Create left menu --------------------------------------//
     const leftMenu = createElement('div', 'left-menu','', '');
+
     // Horizontal div for menu button and name
     const topDivLeftMenu = createElement('div', 'top-div-left-menu','');
     leftMenu.appendChild(topDivLeftMenu);
+
     // Menu retract button
     const menuRetractButton = createElement('button', 'menu-retract-btn', '☰');
     topDivLeftMenu.appendChild(menuRetractButton);
+
     // Name of site
-    const siteName = createElement('p', 'menu-site-name', 'Welcome to ToDo');
+    const siteName = createElement('p', 'menu-site-name', 'Welcome to To-Do');
     topDivLeftMenu.appendChild(siteName);
+
     // Create screen cover for rest of main screen when menu is open
     const screenCover = createElement('div', 'screen-cover', '');
 
+    //--------------------------EventListeners --------------------------------------//
+    // Expand button
     menuExpandButton.addEventListener('click',() => expand(leftMenu, screenCover));
+
+    // Animations
     screenCover.addEventListener('click', (e) => retractAnimation(leftMenu, e.target));
     menuRetractButton.addEventListener('click', () => retractAnimation(leftMenu, screenCover));
+
+    // Extract after animation
     leftMenu.addEventListener('animationend', function(e)
     {
-        console.log(e.animationName)
+        // Check if animation is out
         if(e.animationName === 'slideOut')
             retract(screenCover, leftMenu);
     });
@@ -42,6 +52,8 @@ function expand(leftMenu, screenCover)
     screenCover.classList.add('opIn');
     document.body.appendChild(screenCover);
 }
+
+
 function retractAnimation(leftMenu, screenCover)
 {
     leftMenu.classList.remove('slideIn');
@@ -49,6 +61,7 @@ function retractAnimation(leftMenu, screenCover)
     screenCover.classList.remove('opIn');
     screenCover.classList.add('opOut');
 }
+
 function retract(screenCover, leftMenu)
 {
     leftMenu.classList.remove('slideOut');
