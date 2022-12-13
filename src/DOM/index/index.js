f27a8624bf6245cc5df284fb216f8a7e0fcd55f2
shayshahal <shay840@gmail.com>
2022-12-13 18:23:59 +0200

    add call to make day list

--- src/DOM/index/index.js
+++ src/DOM/index/index.js
@@ -1,10 +1,12 @@
 import './index.css'
 import loadMenu from '/src/DOM/menu/menu.js';
 import loadMain from '/src/DOM/main/main.js';
+import { loadDay } from '../../Logic/user';
 
 if (process.env.NODE_ENV !== 'production') {
     console.log('Looks like we are in development mode!');
   }
 
+loadDay();
 loadMenu();
 loadMain();
