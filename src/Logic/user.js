b2f11fb4e51d1494695d60c519779abc5ba2dd64
shayshahal <shay840@gmail.com>
2022-12-13 18:28:30 +0200

    give addTask and removeTask to user, make day load function

--- src/Logic/user.js
+++ src/Logic/user.js
@@ -1,12 +1,21 @@
 import listFactory from './list';
+import taskFactory from './task';
 
-export function loadUser()
-
+export function loadDay()
+{
+    let day = listFactory('My Day');
+    let all = JSON.parse(localStorage.getItem('All Tasks'));
+    console.log(all);
+    if(all)
+        all.tasks.forEach(task => {if(task.date === '2022-12-11') {addTask(day, task.name, task.desc, task.date, task.prio)}});
+    else
+        localStorage.setItem('All Tasks', JSON.stringify(listFactory('All Tasks')));
+    localStorage.setItem('My Day', JSON.stringify(day));
+}
 export function getList(name)
 {
     if(typeof(localStorage) === 'undefined')
         return listFactory('empty');
-
     return JSON.parse(localStorage.getItem(name));
 }
 
@@ -24,4 +33,25 @@ export function removeList(name)
         return;
     
     localStorage.removeItem(name);
+}
+export function addTask(listName, taskName, taskDesc, taskDate, taskPrio)
+{
+    let list = getList(listName);
+    if(!list || list.tasks.includes(taskName))
+        return false;
+    let task = taskFactory(taskName, taskDesc, taskDate, taskPrio);
+    list.tasks.shift(task);
+    let all = getList('All Tasks');
+    if(!all || all.tasks.includes(taskName))
+        return task;
+    getList('All Tasks').tasks.shift(task);
+    return task;
+}
+export function removeTask(listName, taskName)
+{
+    if(getList(listName).tasks.includes(taskName))
+        return false;
+    getList(listName).tasks.filter(task => task.name !== taskName)
+    return true;
+    
 }
