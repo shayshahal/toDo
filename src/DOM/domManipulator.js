3df0554de49634f9eda0cf1fb9fc1b1e0cad8316
shayshahal <shay840@gmail.com>
2022-12-15 23:54:13 +0200

    moved animation listeners to domMani

--- src/DOM/domManipulator.js
+++ src/DOM/domManipulator.js
@@ -1,5 +1,3 @@
-import {addTask} from "../Logic/user";
-
 export function createElement(eType, id, textContent, className = false) 
 {
     const element = document.createElement(eType);
@@ -23,7 +21,7 @@ export function tabSwitch(name)
 export function createList(list)
 {
     const listContainer = createElement('div', 'list', '');
-    list.tasks.forEach(task => {listContainer.appendChild(createTask(task))});
+    list.tasks.forEach(task => {listContainer.prepend(createTask(task))});
     return listContainer;
 }
 export function createTask(task)
@@ -36,67 +34,38 @@ export function createTask(task)
     const date = createElement('div', '', task.date);
     taskDiv.appendChild(date);
     const prio = createElement('div', '', task.prio);
+    prio.style.color = prio.textContent === 'high' ? 'red' : prio.textContent === 'medium' ? 'yellow' : 'green';
     taskDiv.appendChild(prio);
-    const check = createElement('input', '', '', 'check');
-    check.type = 'checkBox';
-    check.checked = task.isComplete;
-    taskDiv.appendChild(check);
+    const btnsDiv = createElement('div', '','', 'btnsDiv');
+    const checkBtn = createElement('input', '', '', 'check');
+    checkBtn.type = 'checkBox';
+    checkBtn.checked = task.isComplete;
+    btnsDiv.appendChild(checkBtn);
+    const editBtn = createElement('button', '', '✎', 'edit');
+    btnsDiv.appendChild(editBtn);
+    taskDiv.appendChild(btnsDiv);
+
+    
     return taskDiv;
 }
-/*export function createAddForm(listName)
+export function expandWithAni(parent, element, ani)
 {
-    const addTaskDiv = createElement('form', 'add-task', '');
-
-    const taskName = createElement('input', 'task-name', '');
-    addTaskDiv.appendChild(taskName);
-    const taskDesc = createElement('input', 'task-desc', '');
-    addTaskDiv.appendChild(taskDesc);
-    const taskDate = createElement('input', 'task-date', '');
-    taskDate.type = 'date';
-    taskDate.value = '2022-12-11';
-    addTaskDiv.appendChild(taskDate);
-
-    const taskPrioDiv = createElement('form', 'task-prio-div', '');
-    addTaskDiv.appendChild(taskPrioDiv);
-
-    const taskPrioLow = createElement('input', 'task-prio-low', '');
-    taskPrioLow.type = 'radio';
-    taskPrioLow.value = 'low';
-    taskPrioLow.name = 'prio';
-    taskPrioDiv.appendChild(taskPrioLow);
-    const lowLabel = createElement('label', '', 'Low');
-    lowLabel.for = 'task-prio-low';
-    taskPrioDiv.appendChild(lowLabel);
-
-    const taskPrioMed = createElement('input', 'task-prio-med', '');
-    taskPrioMed.type = 'radio';
-    taskPrioMed.value = 'med';
-    taskPrioMed.name = 'prio';
-    taskPrioDiv.appendChild(taskPrioMed);
-    const medLabel = createElement('label', '', 'Medium');
-    medLabel.for = 'task-prio-med';
-    taskPrioDiv.appendChild(medLabel);
-
-    const taskPrioHigh = createElement('input', 'task-prio-high', '');
-    taskPrioHigh.type = 'radio';
-    taskPrioHigh.value = 'high';
-    taskPrioHigh.name = 'prio';
-    taskPrioDiv.appendChild(taskPrioHigh);
-    const highLabel = createElement('label', '', 'High');
-    highLabel.for = 'task-prio-high';
-    taskPrioDiv.appendChild(highLabel);
-
-    const submit = createElement('button', 'task-submit', '+');
-    submit.type = 'button';
-    addTaskDiv.appendChild(submit);
-
-    submit.addEventListener('click', () =>
-    {
-        let prio = taskPrioHigh.Checked ? taskPrioHigh.value : taskPrioMed.Checked ? taskPrioMed.value : taskPrioLow.value;
-        let task = addTask(listName, taskName.value, taskDesc.value, taskDate.value, prio);
-        if(task)
-            createTask(task);
-    });
-    return addTaskDiv;
+    element.classList.toggle(ani);
+    parent.appendChild(element);
 }
-*/
+
+export function retractAnimation(element, ani)
+{
+    var classList = element.classList;
+    while (classList.length > 0) 
+    {
+        classList.remove(classList.item(0));
+    }
+    element.classList.add(ani);
+}
+
+export function removeAfterAni(parent, element, ani)
+{
+    element.classList.remove(ani);
+    parent.removeChild(element);
+}
