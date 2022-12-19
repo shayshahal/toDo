1b3029ed7261dbf01d26c6ebd9d820526a994fad
shayshahal <shay840@gmail.com>
2022-12-19 17:52:55 +0200

    fixed validation check

--- src/Logic/task.js
+++ src/Logic/task.js
@@ -1,7 +1,7 @@
 export default (name, desc, date, prio) => 
 {
     const isComplete = false;
-    if(name === '' || desc === '' || !date)
+    if(name === '' || desc === '')
         return false;
     return {name,desc,date,prio, isComplete};
 };
